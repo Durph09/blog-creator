6492b252b356115b756a92840ca8e367aa547405
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-06-14 21:57:50 -0400

    reducer in post context

--- context/postContext.js
+++ context/postContext.js
@@ -1,70 +1,81 @@
-import React, { useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useCallback, useReducer, useState } from 'react';
 
 const PostsContext = React.createContext({});
 
 export default PostsContext;
 
-export const PostsProvider = ({children}) => {
-    const [posts, setPosts] = useState([]);
-    const [noMorePosts, setNoMorePosts] = useState(false);
-
-    const deletePost = useCallback((postId) => {
-        setPosts(value => {
-            const newPosts = [];
-            value.forEach(post => {
-                if(post._id !== postId) {
-                    newPosts.push(post);
-                }
-            })
-        })
-    }, []);
-
-    const setPostsFromSSR = useCallback((postsFromSSR = []) => {
-        console.log('POST FROM SSR: ', postsFromSSR);
-        //setPosts(postsFromSSR);
-        setPosts ((value) => {
-            const newPosts = [...value];
-            postsFromSSR.forEach((post )=> {
-                const exists = newPosts.find((p) => p._id === post._id);
-                if(!exists) {
-                    newPosts.push(post);
-                }
-            });
-            return newPosts;
-        });
-
-    }, []);
-
-    const getPosts = useCallback(async ({lastPostDate, getNewerPosts = false}) => {
-        const results = await fetch(`/api/getPosts`, {
-            method: "POST",
-            headers: {
-                'content-type' : "application/json"
-            },
-            body: JSON.stringify({lastPostDate, getNewerPosts })
-        });
-        const json = await results.json();
-        const postsResult = json.posts || [];
-        console.log("POSTS RESULTS: ", postsResult )
-        if(postsResult.length < 5) {
-            setNoMorePosts(true);
+function postsReducer(state, action) {
+  switch (action.type) {
+    case 'addPosts': {
+      const newPosts = [...state];
+      action.posts.forEach((post) => {
+        const exists = newPosts.find((p) => p._id === post._id);
+        if (!exists) {
+          newPosts.push(post);
         }
-        setPosts (value =>{
-            const newPosts = [...value];
-            postsResult.forEach(post => {
-                const exists = newPosts.find((p) => p._id === post._id);
-                if(!exists) {
-                    newPosts.push(post);
-                }
-            });
-            return newPosts;
-        });
-    }, []);
-
-
-    return ( 
-    <PostsContext.Provider value={{posts, setPostsFromSSR, getPosts, noMorePosts, deletePost}}>
-        {children}
-        </PostsContext.Provider>
-    )
+      });
+      return newPosts;
+    }
+    case 'deletePost': {
+      const newPosts = [];
+      state.forEach((post) => {
+        if (post._id !== action.postId) {
+          newPosts.push(post);
+        }
+      });
+      return newPosts;
+    }
+    default:
+      return state;
+  }
 }
+
+export const PostsProvider = ({ children }) => {
+  const [posts, dispatch] = useReducer(postsReducer, []);
+  const [noMorePosts, setNoMorePosts] = useState(false);
+
+  const deletePost = useCallback((postId) => {
+    dispatch({
+      type: 'deletePost',
+      postId,
+    });
+  }, []);
+
+  const setPostsFromSSR = useCallback((postsFromSSR = []) => {
+    dispatch({
+      type: 'addPosts',
+      posts: postsFromSSR,
+    });
+  }, []);
+
+  const getPosts = useCallback(
+    async ({ lastPostDate, getNewerPosts = false }) => {
+      const result = await fetch(`/api/getPosts`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ lastPostDate, getNewerPosts }),
+      });
+      const json = await result.json();
+      const postsResult = json.posts || [];
+      if (postsResult.length < 5) {
+        setNoMorePosts(true);
+      }
+      dispatch({
+        type: 'addPosts',
+        posts: postsResult,
+      });
+    },
+    []
+  );
+
+  return (
+    <PostsContext.Provider
+      value={{ posts, setPostsFromSSR, getPosts, noMorePosts, deletePost }}
+    >
+      {children}
+    </PostsContext.Provider>
+  );
+};
