cb2b4c0312c5362d3b77d34510560baf35791298
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-06-21 10:54:30 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-scree h-screen overflow-hidden flex justify-center items-center relative">
-      <Image src="/hero.webp" alt="Hero" fill className="absolute" />
+      <Image src="../public/hero.webp" alt="Hero" fill className="absolute" />
       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
         <Logo />
         <p>
