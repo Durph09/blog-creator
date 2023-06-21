c3bc4d7264b5b67848e9282145537f4df4128b73
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-06-20 22:51:12 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm"> 
   <Logo />
   <p> The AI-powered SAAS solution to fenerate SEO-optimized blog posts in minutes.  Get high-quality content, without sacrificing your time.</p>
-  <Link href="/post/newStream" className="bg-green-500 tracking-wider w-full text-center text-white cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block">Begin</Link>
+  <Link href="/post/new" className="bg-green-500 tracking-wider w-full text-center text-white cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block">Begin</Link>
   </div>
   </div>
   )
