74cf418ce2e10757df06d489e86615d0a76531da
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-07-05 21:07:23 -0400

    hero image imported

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Logo } from "../components/Logo/Logo";
 import Link from "next/link";
+import Hero from "../public/hero.webp"
 
 export default function Home() {
   return (
     <div className="w-scree h-screen overflow-hidden flex justify-center items-center relative">
-      <Image src='/hero.webp' alt="Hero" fill className="absolute" />
+      <Image src={Hero} alt="Hero" fill className="absolute" />
       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
         <Logo />
         <p>
